7fc8d241c9ec67e92fea66554228ed57ae44da3e
Carter Schmalzle <cjschmal00@gmail.com>
2021-02-17 16:53:55 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: "frontend",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
